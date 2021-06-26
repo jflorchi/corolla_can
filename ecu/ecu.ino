1d5a0c85616d00ec62486453d3dbd68ad8589bbf
jflorchi <jordan.florchinger@alumni.uleth.ca>
2021-06-26 15:04:46 -0600

    finished optimizing, removed PCM_CRUISE_SM parsing since physical button has been written

--- ecu/ecu.ino
+++ ecu/ecu.ino
@@ -1,9 +1,12 @@
 #include <CAN.h>
 
-boolean OP_ON = false;
+boolean openEnabled = false;
 uint8_t setSpeed = 0x0;
 boolean blinker_left = false;
 boolean blinker_right = false;
+
+bool lastState = false;
+bool stateChanged = false;
 
 // wheel speeds - front right 1 front right 2 etc... each wheel speed is 2 bytes
 short fr1 = -1, fr2 = -1, fl1 = -1, fl2 = -1, br1 = -1, br2 = -1, bl1 = -1, bl2 = -1;
@@ -13,6 +16,11 @@ uint8_t lkasCounter = 0;
 const uint8_t LEAD_INFO_MSG[8] PROGMEM = {0xff, 0xf8, 0x00, 0x08, 0x7f, 0xe0, 0x00, 0x4e};
 const uint8_t GEAR_MSG[8] PROGMEM = {0x0, 0x0, 0x0, 0x0, 0x0, 0x80, 0x0, 0x0};
 
+uint8_t LKAS_MSG[5] = {0x0, 0x0, 0x0, 0x0, 0x0};
+uint8_t PCM_CRUISE_MSG[8] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+uint8_t PCM_CRUISE_2_MSG[8] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+uint8_t STEERING_LEVER_MSG[8] = {0x29, 0x0, 0x01, 0x0, 0x0, 0x0, 0x76};
+
 /**
     #onReceive
     Processes CAN messages that are needed for translation
@@ -20,10 +28,7 @@ const uint8_t GEAR_MSG[8] PROGMEM = {0x0, 0x0, 0x0, 0x0, 0x0, 0x80, 0x0, 0x0};
 void onReceive(uint8_t packetSize) {
     long id = CAN.packetId();
     
-    if (id == 0x399) {
-        CAN.read();
-        OP_ON = (CAN.read() & 0x2) == 2; // figure this out
-    } else if (id == 0xb0) {
+    if (id == 0xb0) {
         uint8_t ffr1 = CAN.read();
         uint8_t ffr2 = CAN.read();
         uint8_t ffl1 = CAN.read();
@@ -103,10 +108,10 @@ bool writeMsg(uint8_t id, uint8_t *msg, bool checksum) {
     fill the last byte with the checksum of the rest
 */
 void attachChecksum(uint8_t id, uint8_t *msg) {
-    can_cksum(msg, sizeof(msg) - 1, id);
+    getChecksum(msg, sizeof(msg) - 1, id);
 }
 
-int can_cksum(uint8_t *dat, uint8_t len, uint16_t addr) {
+int getChecksum(uint8_t *dat, uint8_t len, uint16_t addr) {
     uint8_t checksum = 0;
     checksum = ((addr & 0xFF00) >> 8) + (addr & 0x00FF) + len + 1;
     for (int ii = 0; ii < len; ii++) {
@@ -115,8 +120,6 @@ int can_cksum(uint8_t *dat, uint8_t len, uint16_t addr) {
     return checksum;
 }
 
-bool lastState = false;
-bool stateChanged = false;
 
 void setup() {
     Serial.begin(9600);
@@ -131,15 +134,15 @@ void setup() {
 
 void loop() {
 
-//  Media Button Handling
+    // Media Button Handling
     bool buttonPressed = digitalRead(8) == 0;
     if (buttonPressed) {
         if (lastState != buttonPressed) {
             if (!stateChanged) {
-                if (OP_ON) {
-                    OP_ON = false;
+                if (openEnabled) {
+                    openEnabled = false;
                 } else {
-                    OP_ON = true;
+                    openEnabled = true;
                 }
             }
             stateChanged = true;
@@ -156,26 +159,26 @@ void loop() {
     writeMsg(0x2e6, LEAD_INFO_MSG, false);
 
     //0x2e4 STERING_LKAS
-    uint8_t zero = 0;
-    uint8_t lkasMsg[5] = { ((zero |= 1UL << 0) & ~0x7F) | (lkasCounter * 0x7F), 0x0, 0x0, 0x0 };
-    writeMsg(0x2e4, lkasMsg, true);
+    LKAS_MSG[0] = ((LKAS_MSG[0] |= 1UL << 0) & ~0x7F) | (lkasCounter * 0x7F);
+    writeMsg(0x2e4, LKAS_MSG, true);
     lkasCounter = (lkasCounter + 1) % 64;
 
     //0x1d2 msg PCM_CRUISE
-    uint8_t pcmCruiseMsg[8] = {(OP_ON << 5) & 0x20, 0x0, 0x0, 0x0, 0x0, (OP_ON << 7) & 0x80};
-    writeMsg(0x1d2, pcmCruiseMsg, true);
+    PCM_CRUISE_MSG[0] = (openEnabled << 5) & 0x20;
+    PCM_CRUISE_MSG[5] = (openEnabled << 7) & 0x80;
+    writeMsg(0x1d2, PCM_CRUISE_MSG, true);
 
     //0x1d3 msg PCM_CRUISE_2
-    uint8_t pcmCruise2Msg[8] = {0x0, (OP_ON << 7) & 0x80 | 0x28, setSpeed, 0x0, 0x0, 0x0, 0x0};
-    writeMsg(0x1d3, pcmCruise2Msg, true);
+    PCM_CRUISE_2_MSG[1] = (openEnabled << 7) & 0x80 | 0x28;
+    PCM_CRUISE_2_MSG[2] = setSpeed;
+    writeMsg(0x1d3, PCM_CRUISE_2_MSG, true);
 
     // 0x3bc msg GEAR_PACKET
-    uint8_t gearMsg[8] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
-    writeMsg(0x3bc, gearMsg, false);
+    writeMsg(0x3bc, GEAR_MSG, false);
 
     //0x614 msg steering_levers
-    uint8_t steeringLeverMsg[8] = {0x29, 0x0, 0x01, (blinker_left << 5) & 0x20 | (blinker_right << 4) & 0x10, 0x0, 0x0, 0x76};
-    writeMsg(0x614, steeringLeverMsg, true);
+    STEERING_LEVER_MSG[3] = (blinker_left << 5) & 0x20 | (blinker_right << 4) & 0x10;
+    writeMsg(0x614, STEERING_LEVER_MSG, true);
 
     delay(8);
 }
