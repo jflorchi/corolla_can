772cd421fba822ec6ca1b6e22d5b46501e768d8e
jflorchi <jordan.florchinger@alumni.uleth.ca>
2021-06-26 08:55:32 -0600

    more optimizations and cleaning

--- ecu/ecu.ino
+++ ecu/ecu.ino
@@ -87,6 +87,34 @@ void onReceive(uint8_t packetSize) {
     }
 }
 
+bool writeMsg(uint8_t id, uint8_t msg[], bool checksum) {
+    CAN.beginPacket(id);
+    if (checksum) {
+        attachChecksum(id, msg);
+    }
+    for (int i = 0; i < sizeof(msg); i++) {
+        CAN.write(msg[i]);
+    }
+    return CAN.endPacket();
+}
+
+/**
+    Leave the last byte of the array empty, then call this to
+    fill the last byte with the checksum of the rest
+*/
+void attachChecksum(uint8_t id, uint8_t msg[]) {
+    can_cksum(msg, sizeof(msg) - 1, id);
+}
+
+int can_cksum(uint8_t *dat, uint8_t len, uint16_t addr) {
+    uint8_t checksum = 0;
+    checksum = ((addr & 0xFF00) >> 8) + (addr & 0x00FF) + len + 1;
+    for (int ii = 0; ii < len; ii++) {
+        checksum += (dat[ii]);
+    }
+    return checksum;
+}
+
 void setup() {
     Serial.begin(9600);
     Serial.println("init");
@@ -119,91 +147,30 @@ void loop() {
 //    }
 
     //0x2e6 LEAD_INFO
-    CAN.beginPacket(0x2e6);
-    for (uint8_t i = 0; i < 8; i++) {
-        CAN.write(LEAD_INFO_MSG[i]);
-    }
+    writeMsg(0x2e6, LEAD_INFO_MSG, false);
 
     //0x2e4 STERING_LKAS
-    uint8_t lkasDat[8];
-    uint8_t val = 0;
-    val = val |= 1UL << 0; // set first bit to zero
-    val = (val & ~0x7F) | (lkasCounter * 0x7F); // set interal 6 bits to counter value
-    lkasDat[0] = val;
-    lkasDat[1] = 0x0;
-    lkasDat[2] = 0x0;
-    lkasDat[3] = 0x0;
-    lkasDat[4] = can_cksum(lkasDat, 4, 0x2e4);
-    CAN.beginPacket(0x2e4);
-    for (uint8_t i = 0; i < 5; i++) {
-        CAN.write(lkasDat[i]);
-    }
-    CAN.endPacket();
+    uint8_t zero = 0;
+    uint8_t lkasMsg[5] = { ((zero |= 1UL << 0) & ~0x7F) | (lkasCounter * 0x7F), 0x0, 0x0, 0x0 };
+    writeMsg(0x2e4, lkasMsg, true);
     lkasCounter = (lkasCounter + 1) % 64;
 
     //0x1d2 msg PCM_CRUISE
-    uint8_t dat[8];
-    dat[0] = (OP_ON << 5) & 0x20;
-    dat[1] = 0x0;
-    dat[2] = 0x0;
-    dat[3] = 0x0;
-    dat[4] = 0x0;
-    dat[5] = 0x0;
-    dat[6] = (OP_ON << 7) & 0x80;
-    dat[7] = can_cksum(dat, 7, 0x1d2);
-    CAN.beginPacket(0x1d2);
-    for (int ii = 0; ii < 8; ii++) {
-        CAN.write(dat[ii]);
-    }
-    CAN.endPacket();
+    uint8_t pcmCruiseMsg[8] = {(OP_ON << 5) & 0x20, 0x0, 0x0, 0x0, 0x0, (OP_ON << 7) & 0x80};
+    writeMsg(0x1d2, pcmCruiseMsg, true);
 
     //0x1d3 msg PCM_CRUISE_2
-    uint8_t dat2[8];
-    dat2[0] = 0x0;
-    dat2[1] = (OP_ON << 7) & 0x80 | 0x28;
-    dat2[2] = setSpeed;
-    dat2[3] = 0x0;
-    dat2[4] = 0x0;
-    dat2[5] = 0x0;
-    dat2[6] = 0x0;
-    dat2[7] = can_cksum(dat2, 7, 0x1d3);
-    CAN.beginPacket(0x1d3);
-    for (int ii = 0; ii < 8; ii++) {
-        CAN.write(dat2[ii]);
-    }
-    CAN.endPacket();
+    uint8_t pcmCruise2Msg[8] = {0x0, (OP_ON << 7) & 0x80 | 0x28, setSpeed, 0x0, 0x0, 0x0, 0x0};
+    writeMsg(0x1d3, pcmCruise2Msg, true);
 
     // 0x3bc msg GEAR_PACKET
-    CAN.beginPacket(0x3bc);
-    for (int ii = 0; ii < 8; ii++) {
-        CAN.write(GEAR_MSG[ii]);
-    }
-    CAN.endPacket();
+    uint8_t gearMsg[8] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+    writeMsg(0x3bc, gearMsg, false);
 
     //0x614 msg steering_levers
-    uint8_t dat614[8];
-    dat614[0] = 0x29;
-    dat614[1] = 0x0;
-    dat614[2] = 0x01;
-    dat614[3] = (blinker_left << 5) & 0x20 | (blinker_right << 4) & 0x10;
-    dat614[4] = 0x0;
-    dat614[5] = 0x0;
-    dat614[6] = 0x76;
-    dat614[7] = can_cksum(dat614, 7, 0x614);
-    CAN.beginPacket(0x614);
-    for (int ii = 0; ii < 8; ii++) {
-        CAN.write(dat614[ii]);
-    }
-    CAN.endPacket();
+    uint8_t steeringLeverMsg[8] = {0x29, 0x0, 0x01, (blinker_left << 5) & 0x20 | (blinker_right << 4) & 0x10, 0x0, 0x0, 0x76};
+    writeMsg(0x614, steeringLeverMsg, true);
 
     delay(8);
 }
 
-int can_cksum(uint8_t *dat, uint8_t len, uint16_t addr) {
-    uint8_t checksum = 0;
-    checksum = ((addr & 0xFF00) >> 8) + (addr & 0x00FF) + len + 1;
-    for (int ii = 0; ii < len; ii++) {
-        checksum += (dat[ii]);
-    }
-    return checksum;
-}
